ddc698b0702de759b49c8e3e0afd0d61271833dd
Ayush Bamrara <ayushbamrara10@gmail.com>
2025-07-07 23:04:14 +0530

    Tailwind setup

---
+++ src/components/index.jsx
@@ -0,0 +1,5 @@
+export { Header } from "./header";
+export { Footer } from "./footer";
+
+export { Card } from "./Card";
+ 
---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,9 @@
+export const MovieList = () => {
+  return (
+    <main>
+        <div className="italic">
+            MovieList
+        </div>
+    </main>
+  )
+}
